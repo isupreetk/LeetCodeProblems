f1ad662b096acd701beebf1c67caa60eab3efce1
supreetkaur <supreet97@gmail.com>
2024-02-02 15:07:11 -0500

    2nd approach using for loops

--- ReverseVowelsOfAString/ReverseVowelsOfAString.js
+++ ReverseVowelsOfAString/ReverseVowelsOfAString.js
@@ -1,56 +1,38 @@
-/**
- * @param {string} s
- * @return {string}
- */
+// Approach 2
 
-// 1st solution
-
-var reverseVowels = function (s) {
-  let vowelsList = ["a", "e", "i", "o", "u"];
-  let ourVowels = [];
-  let arr = s.split("");
-  for (let i = 0; i < arr.length; i++) {
-    if (vowelsList.includes(arr[i])) {
-      ourVowels.push(arr[i]);
-    }
-  }
-  for (let k = 0; k < arr.length; k++) {
-    if (ourVowels.includes(arr[k])) {
-      for (let m = 0; m < ourVowels.length; m++) {
-        if (ourVowels[m] === arr[k]) {
-          arr.splice(k, 1, ourVowels[ourVowels.length - m - 1]);
-          k++;
-          m++;
-        }
-      }
-    }
-    s = arr.join("");
-  }
-  return s;
-};
-
-// 2nd Solution
 /**
  * @param {string} s
  * @return {string}
  */
 var reverseVowels = function (s) {
   let vowelsList = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  let ourVowels = [];
   let arr = s.split("");
-  let count_of_replaced_vowels = 0;
-  for (let i = 0; i < arr.length; i++) {
+  let p1 = -1;
+  let p2 = arr.length;
+  let temp;
+  for (let i = 0; i < p2; i++) {
+    console.log("outermost for loop");
+    console.log("i", i);
+    console.log("arr[i]", arr[i]);
     if (vowelsList.includes(arr[i])) {
-      ourVowels.push(arr[i]);
-    }
-  }
-  for (let k = 0; k < arr.length; k++) {
-    if (ourVowels.includes(arr[k])) {
-      arr[k] = ourVowels[ourVowels.length - count_of_replaced_vowels - 1];
-      // arr.splice(k, 1, ourVowels[(ourVowels.length-count_of_replaced_vowels-1)])
-      count_of_replaced_vowels++;
+      p1 = i;
+      console.log("p1", p1);
+      for (let j = p2 - 1; j > i; j--) {
+        console.log("j", j);
+        console.log("arr[j]", arr[j]);
+        if (vowelsList.includes(arr[j])) {
+          p2 = j;
+          console.log("p2", p2);
+          temp = arr[p1];
+          arr[p1] = arr[p2];
+          arr[p2] = temp;
+          break;
+        }
+      }
     }
   }
   s = arr.join("");
   return s;
 };
+
+// Approach 3
