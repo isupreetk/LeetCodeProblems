01b1390810c76d0cb54a0fe0163d886601e6ffa5
supreetkaur <supreet97@gmail.com>
2024-02-04 11:01:32 -0500

    count of replaced vowels approach

--- ReverseVowelsOfAString/ReverseVowelsOfAString.js
+++ ReverseVowelsOfAString/ReverseVowelsOfAString.js
@@ -1,3 +1,29 @@
+// Approach 1
+
+var reverseVowels = function (s) {
+  let vowelsList = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let ourVowels = [];
+  let arr = s.split("");
+  let count_of_replaced_vowels = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (vowelsList.includes(arr[i])) {
+      ourVowels.push(arr[i]);
+    }
+  }
+  for (let k = 0; k < arr.length; k++) {
+    if (ourVowels.includes(arr[k])) {
+      arr.splice(
+        k,
+        1,
+        ourVowels[ourVowels.length - count_of_replaced_vowels - 1]
+      );
+      count_of_replaced_vowels++;
+    }
+  }
+  s = arr.join("");
+  return s;
+};
+
 // Approach 2
 
 /**
