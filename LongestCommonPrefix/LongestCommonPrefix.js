bb0639b38fe95d72710b5abe1d22bd3b64dab0d5
supreetkaur <supreet97@gmail.com>
2024-03-24 09:26:10 -0700

    accepted solution for 14. Longest Common Prefix

--- LongestCommonPrefix/LongestCommonPrefix.js
+++ LongestCommonPrefix/LongestCommonPrefix.js
@@ -2,37 +2,37 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    let minStrLength = 201;
-    let commonLength = 0;
-    let commonPrefix = "";
-    // Find the smallest string in the array
-    for (let i = 0 ; i < strs.length ; i++) {
-        if (minStrLength > strs[i].length) {
-            minStrLength = strs[i].length;
-            console.log("minStrLength", minStrLength);
-        }
+var longestCommonPrefix = function (strs) {
+  let minStrLength = 201;
+  let commonLength = 0;
+  let commonPrefix = "";
+  // Find the smallest string in the array
+  for (let i = 0; i < strs.length; i++) {
+    if (minStrLength > strs[i].length) {
+      minStrLength = strs[i].length;
     }
-    // Find the length of common prefix in all the strings
-    if (strs.length === 1) {
-        commonLength = strs[0].length;
-    }
-    else {
-        for (let k = 0 ; k < minStrLength ; k++) {
-            console.log("k", k);
+  }
+  // Find the length of common prefix in all the strings
+  if (strs.length === 1) {
+    commonLength = strs[0].length;
+  } else {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][0] !== strs[0][0]) {
+        commonLength = 0;
+        break;
+      }
 
-            for (let j = 1 ; j < strs.length ; j++) {
-                    console.log("j", j);
-                    
-                    if (strs[j][k] !== strs[0][k]) {
-                        break;
-                    }
-            
-            }
-        
+      for (let k = 0; k < minStrLength; k++) {
+        if (strs[j][k] !== strs[0][k]) {
+          commonLength = k;
+          minStrLength = k;
+          break;
+        } else {
+          commonLength = k + 1;
         }
+      }
     }
-commonPrefix = strs[0].substring(0,commonLength);
-console.log("commonPrefix", commonPrefix);
-return commonPrefix;
+  }
+  commonPrefix = strs[0].substring(0, commonLength);
+  return commonPrefix;
 };
