527c2f8528b5bf87baa9e4992af085f976db38ad
supreetkaur <supreet97@gmail.com>
2024-04-07 08:18:56 -0700

    2nd attempt for 605. Can Place Flowers

--- CanPlaceFlowers/CanPlaceFlowers.js
+++ CanPlaceFlowers/CanPlaceFlowers.js
@@ -3,39 +3,31 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function (flowerbed, n) {
+var canPlaceFlowers = function(flowerbed, n) {
   let count = 0;
-  let error = 0;
   for (let i = 0; i < flowerbed.length; i++) {
-    // if (flowerbed[i] !== flowerbed[i+1] && (flowerbed[i] === 1 || flowerbed[i+1] === 1 )) {
-    //     count++;
-    //     console.log("count in if",count);
-    // }
-    // else if (!(flowerbed[i] === flowerbed[i+1] && (flowerbed[i] === 0 || flowerbed[i+1] === 0 ))) {
-    //     error++;
-    //     console.log("error in else", error);
-    // }
 
-    if (
-      flowerbed[i] === flowerbed[i + 1] &&
-      flowerbed[i + 1] === flowerbed[i + 2] &&
-      flowerbed[i] === 0 &&
-      flowerbed[i + 1] === 0 &&
-      flowerbed[i + 2] === 0
-    ) {
-      count++;
-      console.log("count in if", count);
-    }
-    // else if (flowerbed[i] !== flowerbed[i+1] && (flowerbed[i] === 1 || flowerbed[i+1] === 1 ))
-    //     error++;
-    //     console.log("error in else", error);
-    // }
-  }
+      if (flowerbed[i+1] === flowerbed[i+2]  && flowerbed[i]=== 1 
+      && flowerbed[i+1] === 0 && flowerbed[i+2] === 0
+      ) {
+          count++;
+          flowerbed[i+1] = 1;
+          console.log("flowerbed", flowerbed);
+      }
+
+      else if (flowerbed[i] === flowerbed[i+1] && flowerbed[i+2]=== 1 
+      && flowerbed[i+1] === 0 && flowerbed[i] === 0
+      ) {
+          count++;
+          flowerbed[i] = 1;
+          console.log("flowerbed", flowerbed);
+      }
 
   if (count === n) {
-    // && error === 0
-    return true;
-  } else {
-    return false;
+      return true;
+  }
+  else {
+      return false;
+  }
   }
 };
