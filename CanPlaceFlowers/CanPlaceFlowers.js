43e505fac5d0dc8b96e5d65c678e364425f108a1
supreetkaur <supreet97@gmail.com>
2024-04-16 13:07:39 -0700

    3rd attempt for 605. Can Place Flowers

--- CanPlaceFlowers/CanPlaceFlowers.js
+++ CanPlaceFlowers/CanPlaceFlowers.js
@@ -3,31 +3,72 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
+var canPlaceFlowers = function (flowerbed, n) {
   let count = 0;
-  for (let i = 0; i < flowerbed.length; i++) {
+  let zeroCount = 0;
+  let left = 0;
+  let right = flowerbed.length - 1;
 
-      if (flowerbed[i+1] === flowerbed[i+2]  && flowerbed[i]=== 1 
-      && flowerbed[i+1] === 0 && flowerbed[i+2] === 0
-      ) {
-          count++;
-          flowerbed[i+1] = 1;
-          console.log("flowerbed", flowerbed);
+  if (n === 0) {
+    return true;
+  } else {
+    for (let i = 0; i < flowerbed.length; i++) {
+      console.log("current index", i);
+      if (flowerbed[i] === 0) {
+        zeroCount++;
+        console.log("zeroCount", zeroCount);
       }
+    }
 
-      else if (flowerbed[i] === flowerbed[i+1] && flowerbed[i+2]=== 1 
-      && flowerbed[i+1] === 0 && flowerbed[i] === 0
-      ) {
-          count++;
-          flowerbed[i] = 1;
-          console.log("flowerbed", flowerbed);
+    if (zeroCount === 2 * n + 1) {
+      for (let i = 0; i < flowerbed.length; i++) {
+        if (
+          flowerbed[i] === flowerbed[i + 1] &&
+          flowerbed[i] === 0 &&
+          flowerbed[i + 1] === 0
+        ) {
+          left = i;
+
+          if (flowerbed[i + 2] === 0) {
+            right = i + 2;
+            count++;
+            i++;
+            console.log("count of can place flowers", count);
+            console.log("index updated", i);
+          } else {
+            count++;
+            i++;
+            console.log("count of can place flowers", count);
+            console.log("index updated", i);
+          }
+        } else if (
+          flowerbed[i + 1] === flowerbed[i + 2] &&
+          flowerbed[i] === 1 &&
+          flowerbed[i + 1] === 0 &&
+          flowerbed[i + 2] === 0
+        ) {
+          left = i + 1;
+
+          if (flowerbed[i + 3] === 0) {
+            right = i + 3;
+            count++;
+            i = +2;
+            console.log("count of can place flowers in else", count);
+            console.log("index updated in else", i);
+          } else {
+            console.log("breaking due to else");
+            break;
+          }
+        }
       }
-
-  if (count === n) {
-      return true;
-  }
-  else {
+    } else {
       return false;
-  }
+    }
+
+    if (count === n) {
+      return true;
+    } else {
+      return false;
+    }
   }
 };
