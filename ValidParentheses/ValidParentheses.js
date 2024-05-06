3372966a16370549eee6df01ceddfe41fd302d12
supreetkaur <supreet97@gmail.com>
2024-05-06 12:26:36 -0700

    completed all test cases for 20. Valid Parentheses

--- ValidParentheses/ValidParentheses.js
+++ ValidParentheses/ValidParentheses.js
@@ -2,64 +2,81 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function (s) {
   let error = false;
-  let openingArray = ["(","{","["];
+  let openingArray = ["(", "{", "["];
   let closingArray = [];
-  
+
   for (let i = 0; i < s.length; i++) {
-      if (s[i] === "(") {
+    if (s[i] === "(") {
+      closingArray.push(")");
 
-          closingArray.push(")");
-
-          if (s[i+1] === closingArray.pop()) {
-              console.log("() completed")
-              i++;
-          }
-
-          else if (openingArray.includes(s[i+1])) {
-              console.log(`new bracket ${s[i+1]} opened`);
-          }   
-      }
-
-      else if (s[i] === "[") {
-
+      if (s[i + 1] === closingArray[closingArray.length - 1]) {
+        i++;
+        closingArray.splice(closingArray.length - 1, 1);
+      } else if (openingArray.includes(s[i + 1])) {
+        if (s[i + 1] === "[") {
           closingArray.push("]");
-          
-          if (s[i+1] === closingArray.pop()) {
-              console.log("[] completed")
-              i++;
-          }
-
-          else if (openingArray.includes(s[i+1])) {
-              console.log(`new bracket ${s[i+1]} opened`);
-          }
-      }
-
-      else if (s[i] === "{") {
-
+        } else if (s[i + 1] === "{") {
           closingArray.push("}");
-
-          if (s[i+1] === closingArray.pop()) {
-              console.log("{} completed")
-              i++;
-          }
-
-          else if (openingArray.includes(s[i+1])) {
-              console.log(`new bracket ${s[i+1]} opened`);
-          }
+        } else if (s[i + 1] === "(") {
+          closingArray.push(")");
+        }
+        i++;
+      } else if (i === s.length - 1) {
+        error = true;
       }
+    } else if (s[i] === "[") {
+      closingArray.push("]");
 
-      else {
-          error = true;
-          break;
+      if (s[i + 1] === closingArray[closingArray.length - 1]) {
+        i++;
+        closingArray.splice(closingArray.length - 1, 1);
+      } else if (openingArray.includes(s[i + 1])) {
+        if (s[i + 1] === "(") {
+          closingArray.push(")");
+        } else if (s[i + 1] === "{") {
+          closingArray.push("}");
+        } else if (s[i + 1] === "[") {
+          closingArray.push("]");
+        }
+        i++;
+      } else if (i === s.length - 1) {
+        error = true;
       }
+    } else if (s[i] === "{") {
+      closingArray.push("}");
+
+      if (s[i + 1] === closingArray[closingArray.length - 1]) {
+        i++;
+        closingArray.splice(closingArray.length - 1, 1);
+      } else if (openingArray.includes(s[i + 1])) {
+        if (s[i + 1] === "(") {
+          closingArray.push(")");
+        } else if (s[i + 1] === "[") {
+          closingArray.push("]");
+        } else if (s[i + 1] === "{") {
+          closingArray.push("}");
+        }
+        i++;
+      } else if (i === s.length - 1) {
+        error = true;
+      }
+    } else if (s[i] !== closingArray[closingArray.length - 1]) {
+      error = true;
+      break;
+    } else {
+      closingArray.pop();
+    }
   }
 
-  if (error) {
-      return false;
+  if (closingArray.length >= s.length) {
+    error = true;
   }
-  else if (closingArray.length === 0) {
-      return true;
+
+  if (error || closingArray.length !== 0) {
+    return false;
+  } else if (closingArray.length === 0) {
+    return true;
   }
 };
