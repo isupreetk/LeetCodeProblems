8e30cc3b429bbb5277b2e52318ef60f44bb59885
supreetkaur <supreet97@gmail.com>
2024-05-06 11:21:05 -0700

    used array push and pop to check closing brackets

--- ValidParentheses/ValidParentheses.js
+++ ValidParentheses/ValidParentheses.js
@@ -2,26 +2,64 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {
+var isValid = function(s) {
   let error = false;
-  for (let i = 0; i < s.length - 1; i++) {
-    if (s[i] === "(" && s[i + 1] === ")") {
-      console.log("() completed");
-      i++;
-    } else if (s[i] === "[" && s[i + 1] === "]") {
-      console.log("[] completed");
-      i++;
-    } else if (s[i] === "{" && s[i + 1] === "}") {
-      console.log("{} completed");
-      i++;
-    } else {
-      error = true;
-      break;
-    }
+  let openingArray = ["(","{","["];
+  let closingArray = [];
+  
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === "(") {
+
+          closingArray.push(")");
+
+          if (s[i+1] === closingArray.pop()) {
+              console.log("() completed")
+              i++;
+          }
+
+          else if (openingArray.includes(s[i+1])) {
+              console.log(`new bracket ${s[i+1]} opened`);
+          }   
+      }
+
+      else if (s[i] === "[") {
+
+          closingArray.push("]");
+          
+          if (s[i+1] === closingArray.pop()) {
+              console.log("[] completed")
+              i++;
+          }
+
+          else if (openingArray.includes(s[i+1])) {
+              console.log(`new bracket ${s[i+1]} opened`);
+          }
+      }
+
+      else if (s[i] === "{") {
+
+          closingArray.push("}");
+
+          if (s[i+1] === closingArray.pop()) {
+              console.log("{} completed")
+              i++;
+          }
+
+          else if (openingArray.includes(s[i+1])) {
+              console.log(`new bracket ${s[i+1]} opened`);
+          }
+      }
+
+      else {
+          error = true;
+          break;
+      }
   }
+
   if (error) {
-    return false;
-  } else {
-    return true;
+      return false;
+  }
+  else if (closingArray.length === 0) {
+      return true;
   }
 };
