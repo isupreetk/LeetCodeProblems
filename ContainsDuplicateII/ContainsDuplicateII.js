09ff8dd4710b44018fa699944a4a362aea5e8bc2
supreetkaur <supreet97@gmail.com>
2024-05-10 12:01:19 -0600

    completed all testcases for 219. Contains Duplicate II

--- ContainsDuplicateII/ContainsDuplicateII.js
+++ ContainsDuplicateII/ContainsDuplicateII.js
@@ -5,21 +5,20 @@
  */
 var containsNearbyDuplicate = function (nums, k) {
   let hm = new Map();
-  let diff = 0;
+  let diff;
 
   for (let i = 0; i < nums.length; i++) {
     if (hm.has(nums[i])) {
-      console.log("nums[i]", nums[i]);
-      console.log("hm.get(nums[i])", hm.get(nums[i]));
       diff = i - hm.get(nums[i]);
-      console.log("diff", diff);
       hm.set(nums[i], i);
     } else {
       hm.set(nums[i], i);
-      console.log("hm", hm);
-      console.log("i", i);
+    }
+    if (diff < k || diff == k) {
+      break;
     }
   }
+
   if (diff < k || diff == k) {
     return true;
   } else {
