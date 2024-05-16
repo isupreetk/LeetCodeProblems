7ad3b8f5381a5df21f5e84d9b432357fb8820a5f
supreetkaur <supreet97@gmail.com>
2024-05-15 18:19:03 -0600

    completed 58. Length of Last Word

--- LengthOfLastWord/LengthOfLastWord.js
+++ LengthOfLastWord/LengthOfLastWord.js
@@ -5,11 +5,9 @@
 var lengthOfLastWord = function (s) {
   let count = 0;
   for (let i = s.length - 1; i >= 0; i--) {
-    console.log("s[i]", s[i]);
     if (s[i] !== " ") {
       count++;
-      console.log("count", count);
-    } else {
+    } else if (count > 0 && s[i] === " ") {
       break;
     }
   }
