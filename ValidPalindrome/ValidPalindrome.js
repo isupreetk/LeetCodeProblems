c1bc7e5fbe3f7d81cefd36d9bc6c5525aee2365c
supreetkaur <supreet97@gmail.com>
2024-05-16 15:05:34 -0600

    refactored code for 125. Valid Palindrome - no usage of inbuilt functions

--- ValidPalindrome/ValidPalindrome.js
+++ ValidPalindrome/ValidPalindrome.js
@@ -1,3 +1,5 @@
+// ----------- Attempt 1 ------------
+
 /**
  * @param {string} s
  * @return {boolean}
@@ -18,6 +20,48 @@ var isPalindrome = function (s) {
     } else {
       error = true;
       break;
+    }
+  }
+
+  if (error) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// ----------- Attempt 2 ------------
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  let p1 = 0;
+  let p2 = s.length - 1;
+  let error = false;
+
+  while (p1 < p2 || p1 == p2) {
+    let p1AlphaNumericCheck =
+      (s[p1].charCodeAt(0) >= 97 && s[p1].charCodeAt(0) <= 122) ||
+      (s[p1].charCodeAt(0) >= 48 && s[p1].charCodeAt(0) <= 57);
+    let p2AlphaNumericCheck =
+      (s[p2].charCodeAt(0) >= 97 && s[p2].charCodeAt(0) <= 122) ||
+      (s[p2].charCodeAt(0) >= 48 && s[p2].charCodeAt(0) <= 57);
+
+    if (p1AlphaNumericCheck && p2AlphaNumericCheck) {
+      if (s[p1] === s[p2]) {
+        p1++;
+        p2--;
+      } else {
+        error = true;
+        break;
+      }
+    } else if (!p1AlphaNumericCheck) {
+      p1++;
+    } else if (!p2AlphaNumericCheck) {
+      p2--;
     }
   }
 
