3045d825a42b5d8043242b31885ff961df7fb92a
supreetkaur <supreet97@gmail.com>
2024-05-21 14:48:50 -0500

    fix for multiple occurences of second word's first letter in first word

--- LengthOfPrefixSuffixInTwoWords /LengthOfPrefixSuffixInTwoWords.js
+++ LengthOfPrefixSuffixInTwoWords /LengthOfPrefixSuffixInTwoWords.js
@@ -45,42 +45,49 @@ const findCommonLength = (word1, word2) => {
   let potentialStart;
   let word1Char;
   let word2Char;
-  let count = 0;
+  let count;
   let allIndices = [];
   let error = false;
 
   for (let k = 0; k < word1.length; k++) {
+    console.log("word2[0]", word2[0]);
+    console.log("word1[k]", word1[k]);
     if (word1[k] === word2[0]) {
       allIndices.push(k);
     }
-    console.log("allIndices", allIndices);
+  }
 
-    allIndices.forEach((index) => {
-      console.log("allIndices index", index);
-      potentialStart = index;
-      console.log("potentialStart", potentialStart);
+  console.log("allIndices", allIndices);
 
-      for (let i = potentialStart; i < word1.length; i++) {
-        console.log("word1 index", i);
-        word1Char = word1[i];
-        console.log("word1Char", word1Char);
-        word2Char = word2[count];
+  allIndices.forEach((index) => {
+    console.log("allIndices triggered");
+    count = 0;
+    console.log("allIndices index", index);
+    potentialStart = index;
+    console.log("potentialStart", potentialStart);
+
+    for (let i = potentialStart; i < word1.length; i++) {
+      console.log("word1 index", i);
+      word1Char = word1[i];
+      console.log("word1Char", word1Char);
+      word2Char = word2[count];
+      console.log("count", count);
+      console.log("word2Char", word2Char);
+
+      if (word1Char === word2Char) {
+        count++;
         console.log("count", count);
-        console.log("word2Char", word2Char);
-
-        if (word1Char === word2Char) {
-          count++;
-          console.log("count", count);
-        } else {
-          error = true;
-        }
+        error = false;
+      } else {
+        error = true;
+        break;
       }
-      if (error) {
-        return false;
-      }
-    });
-    if (!error) {
-        return count;
-      }
+    }
+  });
+  if (error) {
+    return false;
+  }
+  if (!error) {
+    return count;
   }
 };
