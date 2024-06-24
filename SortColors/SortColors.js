2579a15d0009897566d1e95526ce0c2c54e2ece1
supreetkaur <supreet97@gmail.com>
2024-06-24 15:03:31 -0400

    used comparator function for sorting

--- SortColors/SortColors.js
+++ SortColors/SortColors.js
@@ -2,7 +2,25 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+var compareColors = function (k, l) {
+  let temp = 0;
+  if (k > l) {
+    temp = k;
+    k = l;
+    l = temp;
+  }
+  return [k, l];
+};
+
 var sortColors = function (nums) {
-  let res = nums.sort();
-  return res;
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      res = compareColors(nums[i], nums[j]);
+      nums[i] = res[0];
+      nums[j] = res[1];
+    }
+  }
+  return nums;
 };
