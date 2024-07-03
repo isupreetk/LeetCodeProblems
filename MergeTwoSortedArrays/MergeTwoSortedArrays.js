e23234ade899c7efa4a5e48fc628e641c92f45e6
supreetkaur <supreet97@gmail.com>
2024-07-03 15:28:42 -0400

    Scaler DSA: Sorting 1 - Q1. Merge Two Sorted Arrays

---
+++ MergeTwoSortedArrays/MergeTwoSortedArrays.js
@@ -0,0 +1,30 @@
+//param A : array of integers
+//param B : array of integers
+//return a array of integers
+function solve(A, B) {
+  let arr = [];
+  let countA = 0;
+  let countB = 0;
+  for (let i = 0; i < A.length + B.length - 1; i++) {
+    if (A[countA] <= B[countB]) {
+      arr[i] = A[countA];
+      countA++;
+    } else if (A[countA] > B[countB]) {
+      arr[i] = B[countB];
+      countB++;
+    }
+
+    if (countA >= A.length) {
+      if (countB < B.length) {
+        arr.push(B[countB]);
+        countB++;
+      }
+    } else if (countB >= B.length) {
+      if (countA < A.length) {
+        arr.push(A[countA]);
+        countA++;
+      }
+    }
+  }
+  return arr;
+}
