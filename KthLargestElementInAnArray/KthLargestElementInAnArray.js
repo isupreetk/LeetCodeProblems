c62cf929c7137a92e1fef2c89b5e1e83e19bb69a
supreetkaur <supreet97@gmail.com>
2024-07-05 15:32:16 -0400

    added problem description

--- KthLargestElementInAnArray/KthLargestElementInAnArray.js
+++ KthLargestElementInAnArray/KthLargestElementInAnArray.js
@@ -1,3 +1,10 @@
+/*
+Problem Description
+    Given an integer array nums and an integer k, return the kth largest element in the array.
+    Note that it is the kth largest element in the sorted order, not the kth distinct element.
+    Can you solve it without sorting?
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} k
