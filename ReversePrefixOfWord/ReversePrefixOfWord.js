f46315c05cf660170225553408c10341f995e2a9
supreetkaur <supreet97@gmail.com>
2024-08-28 20:40:59 +0530

    solved 2000. Reverse Prefix of Word

--- ReversePrefixOfWord/ReversePrefixOfWord.js
+++ ReversePrefixOfWord/ReversePrefixOfWord.js
@@ -1,35 +1,33 @@
+/* Problem Statement -
+    Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+    For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+    Return the resulting string.
+*/
+
 /**
  * @param {string} word
  * @param {character} ch
  * @return {string}
  */
 var reversePrefix = function (word, ch) {
-  let chIndex;
-  let indexFound = false;
-  let temp = "";
-  let result;
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === ch) {
-      chIndex = i;
-      indexFound = true;
-    }
-    if (indexFound) {
+  let wordArr = word.split("");
+  let countInd;
+  let temp;
+  for (let i = 0; i < wordArr.length; i++) {
+    if (wordArr[i] === ch) {
+      countInd = i;
       break;
     }
   }
 
-  // logic for string reversal / char swapping
-
-  let arr = word.split("");
-
-  for (let j = 0; j <= chIndex + 1; j++) {
-    while (j < chIndex - j) {
-      temp = arr[chIndex - j];
-      arr[chIndex - j] = arr[j];
-      arr[j] = temp;
-      j++;
-    }
+  for (let j = 0; j <= countInd; j++) {
+    temp = wordArr[j];
+    wordArr[j] = wordArr[countInd];
+    wordArr[countInd] = temp;
+    countInd--;
   }
-  result = arr.join("");
-  return result;
+
+  wordStr = wordArr.join("");
+  return wordStr;
 };
