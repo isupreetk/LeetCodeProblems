8bb7daf9aa96e4da6bce19e9bc3d636abf0551c0
isupreetk <supreet97@gmail.com>
2025-01-23 14:23:46 -0800

    2942. Find Words Containing Character

---
+++ FindWordsContainingCharacter/FindWordsContainingCharacter.js
@@ -0,0 +1,27 @@
+/*
+Problem Description
+
+    LeetCode - 2942. Find Words Containing Character
+
+    Q. You are given a 0-indexed array of strings words and a character x.
+
+    Return an array of indices representing the words that contain the character x.
+
+    Note that the returned array may be in any order.
+*/
+
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+    let result = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) {
+            result.push(i)
+        }
+    }
+    return result;
+};
