0596350960f74b87312767251977bfff57ed355e
isupreetk <supreet97@gmail.com>
2025-01-23 14:54:02 -0800

    optimised solution for 3289. The Two Sneaky Numbers of Digitville

--- TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitville.js
+++ TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitville.js
@@ -8,7 +8,7 @@ Problem Description
     As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
 */
 
-
+// Solution 1
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -32,5 +32,30 @@ var getSneakyNumbers = function (nums) {
             resultArr.push(key);
         }
     })
+    return resultArr;
+};
+
+// Solution 2 - optimises Solution 1 to get rid of second for loop
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getSneakyNumbers = function (nums) {
+    let resultMap = new Map();
+    let resultArr = [];
+    let count;
+    for (let i = 0; i < nums.length; i++) {
+        if (resultMap.get(nums[i]) === undefined) {
+            count = 0;
+        }
+        else {
+            count = resultMap.get(nums[i]);
+        }
+        resultMap.set(nums[i], count + 1);
+
+        if (resultMap.get(nums[i]) === 2) {
+            resultArr.push(nums[i]);
+        }
+    }
     return resultArr;
 };
